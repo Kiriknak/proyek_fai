f7d53cb3204c4268b4f485a7b44a1408eca5a203
ClavesSancti <nicholasgalih00@gmail.com>
2020-06-18 11:59:14 +0700

    buat navbar jadi fixed+right button log/reg

--- index.php
+++ index.php
@@ -4,44 +4,49 @@
 <head>
   <meta charset="utf-8" />
   <title>Starting project</title>
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/>
-  <style>.prompt {
-  transition: all 1s ease-in-out !important;
-    width: 180px;
-}
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
+  <style>
+    .prompt {
+      transition: all 1s ease-in-out !important;
+      width: 180px;
+    }
 
-.prompt:focus, 
-.prompt:hover {
-  width:  calc(350px);
+    .prompt:focus,
+    .prompt:hover {
+      width: 350px;
 
 
-}</style>
+    }
+  </style>
 </head>
 
 <body>
-<div class="ui borderless  menu " >
-      <div class="header item">
-        <h1 class="ui header">NutupLapak.com</h1>
+  <div class="ui borderless  fixed menu ">
+    <div class="item">
+      <h1 class="ui header">NutupLapak.com</h1>
       <!---  <img class="logo" src="assets/images/logo.png">
         Project Name
         --->
-      </div>
-      <a href="#" class="item">Kategori</a>
-      <a class="ui item" href="#">Login/Regiser</a>
-      <div class="right menu">
-      <div class="item">
-      <div class="ui  icon input" id="search">
-        <input class="prompt" type="text" placeholder="Search...">
-        <i class="search link icon"></i>
-      </div>
     </div>
-    
+    <a href="#" class="item">Kategori</a>
+    <div class="item">
+        <div class="ui  icon input" id="search">
+          <input class="prompt" type="text" placeholder="Search...">
+          <i class="search link icon"></i>
+        </div>
       </div>
+    <div class="right menu">
+  <div class="item">
+    <div class="ui basic black button">Login/Sign up</div>
+  </div>
+
+    </div>
   </div>
   <div class=" ui container">
-  
+
   </div>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
 </body>
+
 </html>

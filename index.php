dab84f09e022f02bb9faa827aa3ea6db2118f2a1
ClavesSancti <nicholasgalih00@gmail.com>
2020-06-18 10:42:16 +0700

    add navbar+search

--- index.php
+++ index.php
@@ -1,16 +1,47 @@
 <!DOCTYPE HTML>
 <html>
+
 <head>
-    <meta charset="utf-8" />
-    <title>Starting project</title>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/>
-    
+  <meta charset="utf-8" />
+  <title>Starting project</title>
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/>
+  <style>.prompt {
+  transition: all 1s ease-in-out !important;
+    width: 180px;
+}
+
+.prompt:focus, 
+.prompt:hover {
+  width:  calc(350px);
+
+
+}</style>
 </head>
+
 <body>
-  <div class=" ui container">
-      
+<div class="ui borderless  menu " >
+      <div class="header item">
+        <h1 class="ui header">NutupLapak.com</h1>
+      <!---  <img class="logo" src="assets/images/logo.png">
+        Project Name
+        --->
+      </div>
+      <a href="#" class="item">Kategori</a>
+      <a class="ui item" href="#">Login/Regiser</a>
+      <div class="right menu">
+      <div class="item">
+      <div class="ui  icon input" id="search">
+        <input class="prompt" type="text" placeholder="Search...">
+        <i class="search link icon"></i>
+      </div>
+    </div>
+    
+      </div>
   </div>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+  <div class=" ui container">
+  
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
 </body>
 </html>

fac4615db4460215311f4023c2e4b95961d4e233
ClavesSancti <nicholasgalih00@gmail.com>
2020-06-23 18:31:12 +0700

    use semantic css+ hamburger menu

--- index.php
+++ index.php
@@ -4,49 +4,54 @@
 <head>
   <meta charset="utf-8" />
   <title>Starting project</title>
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
-  <style>
-    .prompt {
-      transition: all 1s ease-in-out !important;
-      width: 180px;
-    }
-
-    .prompt:focus,
-    .prompt:hover {
-      width: 350px;
-
-
-    }
+  <link rel="stylesheet" type="text/css" href="src/semantic.min.css" />
+  <link rel="stylesheet" type="text/css" href="src/hamburger.css">
+  
   </style>
+
 </head>
 
 <body>
-  <div class="ui borderless  fixed menu ">
-    <div class="item">
-      <h1 class="ui header">NutupLapak.com</h1>
-      <!---  <img class="logo" src="assets/images/logo.png">
-        Project Name
-        --->
-    </div>
-    <a href="#" class="item">Kategori</a>
-    <div class="item">
-        <div class="ui  icon input" id="search">
-          <input class="prompt" type="text" placeholder="Search...">
-          <i class="search link icon"></i>
-        </div>
-      </div>
-    <div class="right menu">
+<div class="ui pointing borderless menu  stackable">
+	<a class="item header">
+	NutupLapak.com
+	</a>
+  <a class="item">
+    Home
+  </a>
+  <a class="item">
+    Kategori
+  </a>
   <div class="item">
-    <div class="ui basic black button">Login/Sign up</div>
-  </div>
-
+      <div class="ui transparent icon input">
+        <input type="text" placeholder="Search...">
+        <i class="search link icon"></i>
+      </div>
     </div>
-  </div>
-  <div class=" ui container">
+  <div class="right menu">
+  <div class="item">
+  <div class="ui buttons">
+  <button class="ui green inverted button">Login</button>
+  <div class="or"></div>
+  <button class="ui  reverted green button">Register</button>
+</div>
+    
 
   </div>
+   
+  </div>
+	<div class="hamburger ">
+	    <span class="hamburger-bun"></span>
+	    <span class="hamburger-patty"></span>
+	    <span class="hamburger-bun"></span>
+	</div>
+</div>
+
+  
+  
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+  <script src="src/semantic.min.js"></script>
+  <script src="src/hamburger.js"></script>
 </body>
 
 </html>

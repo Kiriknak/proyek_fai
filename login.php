274db8b263dcbf2af2b676c922c22c8c93fca10b
ClavesSancti <nicholasgalih00@gmail.com>
2020-06-23 19:29:52 +0700

    add login dari fian

---
+++ login.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title>Starting project</title>
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/>
+  <style>.prompt {
+  transition: all 1s ease-in-out !important;
+    width: 180px;
+}
+
+.prompt:focus, 
+.prompt:hover {
+  width:  calc(350px);
+
+
+}</style>
+</head>
+
+<body>
+<div class="ui container">
+<div class="ui center placeholder segment">
+
+  
+<center> 
+<h1>Sign In</h1>
+</center>
+  <div class="ui one column very relaxed stackable grid">
+    <div class="column">
+      <div class="ui form">
+        <div class="field">
+          <label>Username</label>
+          <div class="ui left icon input">
+            <input type="text" placeholder="Username">
+            <i class="user icon"></i>
+          </div>
+        </div>
+        <div class="field">
+          <label>Password</label>
+          <div class="ui left icon input">
+            <input type="password" placeholder="Password">
+            <i class="lock icon"></i>
+          </div>
+        </div>
+        <div class="ui black submit button">Login</div>
+      </div>
+    </div>
+    
+  </div>
+	
+  <div class="ui horizontal divider">
+    Or
+  </div>
+<div class="middle aligned column">
+
+	<center>
+		<br>
+	<lable>Anda Belum Mempunyai Akun??</lable>
+</center>
+</br>
+		<br>
+	<div><a href="daftar.php" class = "ui black submit button">Sign Up</a></div>	
+	
+</div>
+<br>
+	<br>
+<div class="ui  divider">
+</div>
+<br>
+<br>
+<center>
+<div class="footer">
+	<div class = "ui raised segment">
+		
+<lable>2020 @CopyRight NutupLaku.com </lable>
+	</div>
+	</div>
+</center>
+</br>
+<br>
+</div>
+
+  
+</div>
+  
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+</body>
+</html>
